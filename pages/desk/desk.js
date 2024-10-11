29325316f644191ecdc9026667b46f68555cc81f
GaeBolga <benjamin.lefort@mail.com>
2024-10-11 16:04:05 +0200

    ajout d'un bouton de navigation vers classroom

--- pages/desk/desk.js
+++ pages/desk/desk.js
@@ -4,13 +4,22 @@
 document.addEventListener("DOMContentLoaded", function() {
   const button = document.querySelector('.tuto-button');
   const hiddenButton = document.querySelector('.hidden-button');
-
+  const arrow = document.querySelector('.arrow-img')
 
 button.addEventListener("click", function() {
   button.style.display = 'none';
   hiddenButton.style.display = 'block';
+
 });
+
 
 hiddenButton.addEventListener("click", function(){
   hiddenButton.style.display = 'none';
-});})
+  arrow.style.display = 'block';
+});
+
+arrow.addEventListener("click", function(){
+  window.location.href = "http://www.w3schools.com";
+})
+
+})
