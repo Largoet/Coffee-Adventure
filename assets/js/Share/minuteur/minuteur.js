55611d542b54fd7e145566e70d0bf024637e1789
GaeBolga <benjamin.lefort@mail.com>
2024-10-16 14:23:24 +0200

    ajout du fichier sonicdrown.mp3 et mise à jour du minuteur commun

--- assets/js/Share/minuteur/minuteur.js
+++ assets/js/Share/minuteur/minuteur.js
@@ -1,11 +1,17 @@
-let dizaine = 5;
-let unites = 9;
-let minutes = 9;
 
+let dizaine = 0;
+let unites = 1  ;
+let minutes = 1;
+
+const styleTimer = document.querySelector(".timer-container")
 const timerMinutes = document.querySelector("#timer-min");
 const timerSecondsUnites = document.querySelector("#timer-sec-unites");
 const timerSecondsDizaine = document.querySelector("#timer-sec-dizaine");
+
 timerMinutes.innerHTML = minutes;
+timerSecondsDizaine.innerHTML = dizaine;
+timerSecondsUnites.innerHTML = unites;
+
 function startTimer() {
   setInterval(function() {
     unites--;
@@ -24,12 +30,19 @@ function startTimer() {
       timerSecondsUnites.innerHTML = unites;
       timerSecondsDizaine.innerHTML = dizaine;
       timerMinutes.innerHTML = minutes;
+      if (minutes == 0) {
+       
+        setInterval(() => {
+          styleTimer.classList.toggle('colorred');
+          if (minutes == 0 && dizaine == 1 && unites == 3 ){
+            const alertSound = document.getElementById("timer-alert");
+            alertSound.play();
+           }
+        }, 1000);
+      }
     } else if (minutes == 0 && dizaine == 0 && unites == 0) {
       window.location.href = "https://img.freepik.com/vecteurs-libre/game-over-effet-glitch_225004-661.jpg?t=st=1729001229~exp=1729004829~hmac=ca5442e3bdf4d27878606bdc20748629afa9c20f7661ab45f4a5c3d2f664dc8c&w=1380";
     }
   }, 1000);
 }
-
-startTimer();
-
 
